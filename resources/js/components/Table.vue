6749d918856b4af3a2f1da7d1a430c79587d05c1
ZaaK/ <68238387+ZaaKSCR@users.noreply.github.com>
2023-07-17 16:16:48 -0300

    Implementando botoes de visualizar, remover e atualizar

--- resources/js/components/Table.vue
+++ resources/js/components/Table.vue
@@ -4,6 +4,7 @@
             <thead>
                 <tr>
                     <th scope="col" v-for="t, key in titulos" :key="key">{{ t.titulo}}</th>
+                    <th v-if="atualizar || remover || visualizar"> Ações </th>
                 </tr>
             </thead>
             <tbody>
@@ -15,6 +16,11 @@
                             <img :src="'/storage/'+valor" width="100px" height="100px">
                         </span>
                     </td>
+                    <td>
+                        <button v-if="visualizar" class="btn btn-outline-primary btn-sm">Visualizar</button>
+                        <button v-if="atualizar" class="btn btn-outline-success btn-sm">Atualizar</button>
+                        <button v-if="remover" class="btn btn-outline-danger btn-sm">Remover</button>
+                    </td>
                 </tr>
             </tbody>
         </table>
@@ -23,18 +29,18 @@
 
 <script>
     export default {
-        props: ['titulos', 'dados'],
+        props: ['titulos', 'dados', 'atualizar', 'visualizar', 'remover'],
         computed: {
             dadosFiltrados() {
                 let campos = Object.keys(this.titulos);
                 let dadosFiltrados = [];
-                
+
                 this.dados.map((item, chave) => {
                     let itemFiltrado = {}
                     campos.forEach(campo => {
                         itemFiltrado[campo] = item[campo] // Utilizar a sintaxe de array para atribuir valores a objetos
                     });
-                dadosFiltrados.push(itemFiltrado); 
+                dadosFiltrados.push(itemFiltrado);
                 });
                 return dadosFiltrados; // variavel, nao função
             }
